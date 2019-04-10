533a30876baedae2f08e25a0dfc24bbddb476219
shi <shi@shide-MBP.Home>
2019-04-10 23:04:53 +0800

    kill shiSeeder and create the bootstrap in welcome

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"crossorigin="anonymous">
         <title>Hello Laravel</title>
 
         <!-- Fonts -->
@@ -78,22 +78,41 @@
                     @endauth
                 </div>
             @endif
+                <div class="row flex-center">
+                    <div class="content">
+                        <div class="title m-b-md">
+                            Hello Laravel
+                        </div>
 
-            <div class="content">
-                <div class="title m-b-md">
-                    Hello Laravel
+
+                        <div class="links">
+                            <a href="https://laravel.com/docs">Docs</a>
+                            <a href="https://laracasts.com">Laracasts</a>
+                            <a href="https://laravel-news.com">News</a>
+                            <a href="https://blog.laravel.com">Blog</a>
+                            <a href="https://nova.laravel.com">Nova</a>
+                            <a href="https://forge.laravel.com">Forge</a>
+                            <a href="https://github.com/laravel/laravel">GitHub</a>
+                        </div>
+                    </div>
+                    <div class="row flex-center">
+                        <div class="col-2 text-black-20 text-truncate">
+                            Ambitioni dedisse scripsisse iudicaretur.
+                            Cras mattis iudicium purus sit amet fermentum.
+                            Donec sed odio operae, eu vulputate felis rhoncus.
+                        </div>
+                        <span class="d-inline-block text-black-20  text-truncate" style="max-width: 350px;">
+                            Praeterea iter est quasdam res quas ex communi.
+                            At nos hinc posthac, sitientis piros Afros.
+                            Petierunt uti sibi concilium totius Galliae in diem certam indicere.
+                            Cras mattis iudicium purus sit amet fermentum.
+                        </span>
+                    </div>
+
+                </div>
                 </div>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
-            </div>
+
         </div>
     </body>
 </html>
